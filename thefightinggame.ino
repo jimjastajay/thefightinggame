fc283a56e9187993d10aeeb2c51989928cc0141d
Shawn Pierre : Desktop <shawn@origaminc.com>
2020-08-25 20:43:34 -0400

    Basic architecture; Double click changes color; single reduces health; triple resets

--- thefightinggame.ino
+++ thefightinggame.ino
@@ -1,9 +1,61 @@
+enum TileState {NONE, CHOOSING, INFIGHT, DEAD};
+
+byte health = 6;
+
+struct FighterType {
+  Color color;
+};
+
+FighterType one = {RED};
+FighterType two = {YELLOW};
+FighterType three = {GREEN};
+FighterType four = {MAGENTA};
+FighterType fighterList[4] = {{one}, {two}, {three}, {four}};
+byte currentType = -1;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  resetTile();
+}
+
+void nextColor() {
+  currentType += 1;
+  if (currentType >= (sizeof(fighterList) / sizeof(fighterList[0]))) {
+    currentType = 0;
+  }
+  setColor(fighterList[currentType].color);
+}
+
+void decreaseHealth() {
+
+  if (health > 0){
+    health -= 1;
+    for(int i = 5; i >= health; i--){
+      setColorOnFace(OFF, i);
+    }
+  }    
 
 }
 
+void resetTile(){
+  health = 6;
+  currentType = -1;
+  setColor(WHITE);
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if (buttonDoubleClicked()) {
+    nextColor();
+  }
+  if(buttonSingleClicked()){
+    decreaseHealth();
+  }
+
+  if(buttonMultiClicked()){
+    if(buttonClickCount() == 3){
+      resetTile();
+    }
+  }
 
 }
