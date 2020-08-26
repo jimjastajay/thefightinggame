2f375ef71d6631ea59e33420da80b2889970eb76
Shawn Pierre : Laptop <shawn@origaminc.com>
2020-08-26 19:35:52 -0400

    can determine configuration type

--- thefightinggame.ino
+++ thefightinggame.ino
@@ -1,6 +1,16 @@
-enum TileState {NONE, CHOOSING, INFIGHT, DEAD};
+enum TileStates {NONE, CHOOSING, INFIGHT, DEAD};
+enum FighterConfigs {NOCONFIG, BALANCED, ATTACK, DEFENSE};
+
+byte tileState = NONE;
+byte fighterConfig = NOCONFIG;
 
 byte health = 6;
+
+int faceOne = -1;
+int faceTwo = -2;
+
+#define PURPLE makeColorRGB(105, 5, 163)
+#define GREY makeColorRGB(128, 128, 128)
 
 struct FighterType {
   Color color;
@@ -19,6 +29,7 @@ void setup() {
   resetTile();
 }
 
+//Moves to the next color/fighter
 void nextColor() {
   currentType += 1;
   if (currentType >= (sizeof(fighterList) / sizeof(fighterList[0]))) {
@@ -27,39 +38,69 @@ void nextColor() {
   setColor(fighterList[currentType].color);
 }
 
+//Lowers the fighter's health by 1
 void decreaseHealth() {
-
   if (health > 0) {
     health -= 1;
     for (int i = 5; i >= health; i--) {
       setColorOnFace(OFF, i);
     }
   }
-
 }
 
+//Resets the tile to it's basic setting
 void resetTile() {
   health = 6;
   currentType = -1;
   setColor(WHITE);
 }
 
-void markReady() {
-
-}
-
+//Lights up the number of current connections
 void getConnections() {
   int count = 0;
+  faceOne = -1;
+  faceTwo = -1;
+  currentNumberOfConnections = 0;
   setColor(OFF);
+
   FOREACH_FACE(f) {
     if (!isValueReceivedOnFaceExpired(f)) {//a neighbor!
-      count += 1;
-      setColorOnFace(CYAN, f);
+      if (faceOne == -1) {
+        count += 1;
+        faceOne = f;
+        setColorOnFace(CYAN, faceOne);
+      }
+      else {
+        if (faceTwo == -1) {
+          count += 1;
+          faceTwo = f;
+          setColorOnFace(CYAN, faceTwo);
+        }
+      }
     }
   }
   currentNumberOfConnections = count;
+  setConfiguration();
+}
 
-  
+void setConfiguration() {
+
+  if (currentNumberOfConnections == 2) {
+    int distance = abs(faceOne - faceTwo);
+    if (distance == 3) {
+      fighterConfig = ATTACK;
+      setColor(ORANGE);
+    }
+    else if (distance == 1 || distance == 5) {
+      fighterConfig = DEFENSE;
+      setColor(BLUE);
+    }
+    else {
+      fighterConfig = BALANCED;
+      setColor(PURPLE);
+    }
+  }
+
 }
 
 void loop() {
